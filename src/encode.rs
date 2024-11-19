18cce5d96d4705a92f0d057955053c4a30a2386e
bartick <bartickmaiti1998@gmail.com>
2024-11-20 02:46:56 +0530

    add: better recursive encoding

--- src/encode.rs
+++ src/encode.rs
@@ -2,59 +2,51 @@ use crate::types::RLPItem;
 
 fn encode_length(length: usize, offset: u8) -> Vec<u8> {
     let length_bytes = length.to_be_bytes();
-    let mut output = vec![offset + length_bytes.len() as u8];
-    output.extend(&length_bytes);
-    output
+    let mut length_bytes_trimmed = length_bytes
+        .iter()
+        .skip_while(|&&x| x == 0)
+        .copied()
+        .collect::<Vec<_>>();
+    length_bytes_trimmed.insert(0, offset + length_bytes_trimmed.len() as u8);
+    length_bytes_trimmed
 }
 
-fn rlp_encode(input: RLPItem) -> Vec<u8> {
-    let mut output: Vec<u8> = Vec::new();
-
-    match input {
-        RLPItem::Str(text) => {
-            let text = text.as_bytes();
-            if text.len() == 1 && text[0] <= 0x7f {
-                output.push(text[0]);
-            } else {
-                let length = text.len();
-                if length < 56 {
-                    output.push(0x80 + length as u8);
-                } else {
-                    output.extend(encode_length(length, 0xb7));
-                }
-                output.extend(text);
-            }
-        }
-        RLPItem::List(list) => {
-            let inner = encode(list);
-            let length = inner.len();
-            if length < 56 {
-                output.push(0xc0 + length as u8);
-            } else {
-                output.extend(encode_length(length, 0xf7));
-            }
-        }
-    }
-
-    output
-}
-
-pub fn encode(input: Vec<RLPItem>) -> Vec<u8> {
-    let mut output: Vec<u8> = Vec::new();
-
-    for item in input {
-        output.extend(rlp_encode(item));
-    }
-
-    // Add the Length Prefix
-    let length = output.len();
-    if length < 56 {
-        output.insert(0, 0xc0 + length as u8);
+fn encode_str(input: Vec<u8>) -> Vec<u8> {
+    if input.len() == 1 && input[0] <= 0x7f {
+        vec![input[0]]
     } else {
-        output = encode_length(length, 0xf7);
-    }
+        let length = input.len();
+        let mut output = Vec::new();
 
+        if length < 56 {
+            output.push(0x80 + length as u8);
+        } else {
+            output.extend(encode_length(length, 0xb7));
+        }
+        output.extend(input);
+        output
+    }
+}
+
+fn encode_list(list: Vec<RLPItem>) -> Vec<u8> {
+    let inner: Vec<u8> = list.into_iter().flat_map(encode).collect();
+    let length = inner.len();
+    let mut output = Vec::new();
+
+    if length < 56 {
+        output.push(0xc0 + length as u8);
+    } else {
+        output.extend(encode_length(length, 0xf7));
+    }
+    output.extend(inner);
     output
+}
+
+pub fn encode(input: RLPItem) -> Vec<u8> {
+    match input {
+        RLPItem::Str(text) => encode_str(text.into_bytes()),
+        RLPItem::List(list) => encode_list(list),
+    }
 }
 
 #[cfg(test)]
@@ -63,14 +55,15 @@ mod tests {
 
     #[test]
     fn test_encode() {
-        let input: Vec<RLPItem> = vec!["hello".into(), "world".into()];
+        let input: RLPItem = vec!["hello".into(), "world".into()].into();
 
         let expected = vec![
             0xcc, 
             0x85, 
             b'h', b'e', b'l', b'l', b'o', 
             0x85, 
-            b'w', b'o', b'r', b'l', b'd'];
+            b'w', b'o', b'r', b'l', b'd'
+        ];
 
         assert_eq!(encode(input), expected);
     }
