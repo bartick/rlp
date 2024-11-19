ce9ae1570b026f4a4b7f1c583bc5507d95066eed
bartick <bartickmaiti1998@gmail.com>
2024-11-20 04:34:45 +0530

    add: support for bytes

--- src/types.rs
+++ src/types.rs
@@ -1,7 +1,20 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum RLPItem {
+    Bytes(Vec<u8>),
     Str(String),
     List(Vec<RLPItem>),
+}
+
+impl From<u8> for RLPItem {
+    fn from(i: u8) -> Self {
+        RLPItem::Bytes(vec![i])
+    }
+}
+
+impl From<&[u8]> for RLPItem {
+    fn from(v: &[u8]) -> Self {
+        RLPItem::Bytes(v.to_vec())
+    }
 }
 
 impl From<&str> for RLPItem {
@@ -19,6 +32,9 @@ impl From<Vec<RLPItem>> for RLPItem {
 impl std::fmt::Display for RLPItem {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
+            RLPItem::Bytes(v) => {
+                write!(f, "{:?}", v)
+            },
             RLPItem::Str(s) => write!(f, "{}", s),
             RLPItem::List(v) => {
                 write!(f, "[")?;
@@ -35,9 +51,12 @@ impl std::fmt::Display for RLPItem {
 impl std::ops::AddAssign for RLPItem {
     fn add_assign(&mut self, other: Self) {
         match self {
+            RLPItem::Bytes(ref mut v) => {
+                v.extend(other.to_string().bytes());
+            },
             RLPItem::List(ref mut v) => {
                 v.push(other);
-            }
+            },
             RLPItem::Str(ref mut v) => {
                 let mut new_v = v.clone();
                 new_v.push_str(&other.to_string());
